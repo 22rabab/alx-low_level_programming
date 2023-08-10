b953a165434e6110618f5588e80c042b825c9be9
22rabab <rababelbaz@outlook.fr>
2023-08-10 15:37:01 -0700

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print the size of various types on the computer it is compiled and run on
+ * main - print the size of various types
  *
  * Return: 0 (Success)
  */
