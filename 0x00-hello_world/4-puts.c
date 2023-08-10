dae1d29ee54ed5b480d21a137898c1ed6fb8bc14
22rabab <rababelbaz@outlook.fr>
2023-08-10 14:52:48 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#!/bin/bash
-#include<studio.h>
+#include <stdio.h>
+/**
+ * main - print a line with puts
+ *
+ * Return: 0 (Succes)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)}
+	return (0)
+}
