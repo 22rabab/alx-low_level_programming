704095cd874b0a75141782fd8c9c91cd6272bacb
22rabab <rababelbaz@outlook.fr>
2023-08-10 15:29:42 -0700

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,11 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
+char a;
+int b;
+long int c;
+long long int d;
+float f;
 printf("Size of a char: %lu byte(s)"
 printf("Size of a char: %lu byte(s)\n", sizeof(a));
 printf("Size of an int: %lu byte(s)\n", sizeof(b));
