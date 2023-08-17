73a67fa12b4877c0f999c04388703abb1e780633
22rabab <rababelbaz@outlook.fr>
2023-08-17 11:03:32 -0700

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,17 +2,18 @@
 
 /**
  * print_numbers - prints the numbers from 0 to 9 followed by a new line
+ *
+ * Return: Always 0 (Success)
  */
 void print_numbers(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i <= 9; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (i ==2 || i == 4)
+		if (num == 2 || num == 4)
 			continue;
-		_putchar(i +8);
+		_putchar(i + 8);
 	}
-
 	_putchar("\n");
 }
