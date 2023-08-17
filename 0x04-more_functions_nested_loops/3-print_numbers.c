fe1809bdc8266369624c8986973a3f2dde213ac5
22rabab <rababelbaz@outlook.fr>
2023-08-17 10:26:28 -0700

    done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - print 0 - 9
- *
+ *               only using _putchar twice
  * Return: Always 0 (Success)
  */
 
@@ -13,6 +13,7 @@ void print_numbers(void)
 	do {
 		_putchar(num + 48);
 		num++;
-	} while (num >= 0 && num <= 9);
+	}
+	while (num >= 0 && num <= 9);
 	_putchar("\n");
 }
