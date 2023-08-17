92530070a9f26d5ca1565965f3da7a8dee0e7681
22rabab <rababelbaz@outlook.fr>
2023-08-17 16:01:54 -0700

    done

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle made of '#' characters
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+	
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,23 +18,23 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			_putchar("FizzBuzz ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			_putchar("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			_putchar("Buzz ");
 		}
 		else
 		{
-			printf("%d ", i);
+			_putchar("%d ", i);
 		}
 	}
 
-	printf("\n");
+	_putchar("\n");
 
 
 	return (0);
