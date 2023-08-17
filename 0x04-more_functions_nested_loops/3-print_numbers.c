768289778e4b3c0adf9fb898f8c04b65a1baf337
22rabab <rababelbaz@outlook.fr>
2023-08-17 10:24:27 -0700

    done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers -print 0 - 9
+ * print_numbers - print 0 - 9
  *
  * Return: Always 0 (Success)
  */
 
 void print_numbers(void)
 {
-	int num = 0
+	int num = 0;
 
 	do {
 		_putchar(num + 48);
